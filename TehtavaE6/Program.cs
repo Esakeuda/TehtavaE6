433a9f5df97344ab1ad2863fe05186a0922e9c70
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-25 09:03:20 +0300

    Add project files.

---
+++ TehtavaE6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ParittomatLuvut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Käydään läpi kaikki luvut väliltä 1-99 ja tulostetaan parittomat luvut
+            for (int luku = 1; luku < 100; luku += 2)
+            {
+                Console.WriteLine(luku);
+            }
+        }
+    }
+}
+
